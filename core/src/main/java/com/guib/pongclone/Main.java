205b48e1a8f86eb3711f2b53ff509f1b66e6069e
Guilherme Rodrigues <guibnuevo@gmail.com>
2025-03-01 03:04:46 -0300

    starting

--- core/src/main/java/com/guib/pongclone/Main.java
+++ core/src/main/java/com/guib/pongclone/Main.java
@@ -6,7 +6,6 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.utils.ScreenUtils;
-import com.badlogic.gdx.audio.Music;
 
 /**
  * {@link ApplicationListener} implementation shared by all platforms.
