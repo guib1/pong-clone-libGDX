e4229f3cc9b6393bcb33ead7c80c1082c778ae57
Guilherme Rodrigues <guibnuevo@gmail.com>
2025-03-11 17:55:38 -0300

    starting adding some code improvements

--- core/src/main/java/com/guib/pongclone/Player.java
+++ core/src/main/java/com/guib/pongclone/Player.java
@@ -3,7 +3,9 @@ package com.guib.pongclone;
 import com.badlogic.gdx.math.Rectangle;
 
 public class Player {
+    private float x;
     private float y;
+    private float velocity;
     public Rectangle rect = new Rectangle();
 
     public void movement(float delta) {
@@ -14,7 +16,13 @@ public class Player {
         return (int) this.y;
     }
 
-    public float editY(float y) {
-        return this.y = y;
+    public void update(float deltaTime) {
+        this.y += this.velocityY * deltaTime;
+        this.rect.setPosition(this.x, this.y);
+    }
+
+    public int score(int goal) {
+        int score = 0;
+        return score += goal;
     }
 }
